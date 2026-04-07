cc07e2b8efac27a646504476093c15eccc11465e
sicktooth <sicktooth003@gmail.com>
2026-04-07 17:25:31 +0100

    finished A List exercise, working on Deep Comparison

--- exercises/chapter-4/js/list.js
+++ exercises/chapter-4/js/list.js
@@ -9,14 +9,27 @@ let least = {
     }
 },
 list = {},
-array2= [1,2,3]
+array2= [1,2,3];
+function listToArray(list) {
+    let array = [];
+    for (let node = list; node; node = node.rest) {
+        // console.log(node.value, node.rest);
+        if (undefined ?? node.value){
+            array.push(node.value);
+        }
+        // there will definitely be an undefined value at the end of the list cuz if checked.. rest ended somewhere and it's an empty object... so it will be undefined. lesson learnt... always check for undefined values in the end of the loop to avoid errors.
+    }
+    return console.log(array);
+    
+}
 function arrayToList(array) {
-    for (let index = array.length - 1; index >= 0; index--) {
-        const element = array[index];
+    for (let index = array.length; index > 0; index--) {
+        const element = array[index - 1];
         list = {value: element, rest: list}
     }
-    console.log(list)
+    return list;
     // lesson learnt... always find a pattern in the problem... rest is the same as value and rest. But the first value is constant
 }
 
 arrayToList(array2)
+listToArray(list)
