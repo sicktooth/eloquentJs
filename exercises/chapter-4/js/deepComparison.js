72e6e1069f3739b9b8c88869603e4b9c4013b875
sicktooth <sicktooth003@gmail.com>
2026-04-13 20:58:23 +0100

    completed chapter 4 exercise... understood with the aid of ai but it came to my notice that I barely know shit

--- exercises/chapter-4/js/deepComparison.js
+++ exercises/chapter-4/js/deepComparison.js
@@ -1,53 +1,21 @@
 
-// function deepEqual(x , y) {
-//     let typeX = typeof(x),
-//         typeY = typeof(y);
-//     console.log(typeX, typeY, x ,y)
-
-//     if ((x == null || undefined) || (y == null || undefined)) {
-//         return console.log('cannot work on null or undefined value')
-//     } else {
-
-//         let deeds0 = Object.keys(x),
-//             deeds1 = Object.keys(y);
-//         console.log(deeds0, deeds1)
-
-//         if (typeX && typeY == 'object') {
-//             console.log(deeds0, deeds1)
-
-//             if (deeds0 === deeds1) {
-//                 return true
-//             } else console.log('news flash')
-        
-//         } else if (typeX === typeY) {
-//             return true
-            
-//         } else return false
-//     }
+function deepEqual(x , y) {
+    let typeX = typeof x,
+        typeY = typeof y;
+    if (x == null || y == null) return false
+    let keysX = Object.keys(x),
+        keysY = Object.keys(y);
+    if (x == y) return true
+    if (typeX !== 'object' || typeY !== 'object') return false
+    if (keysX.length !== keysY.length) return false
+    for (let key of keysX) {
+        if (!keysY.includes(key) || !deepEqual(x[key], y[key])) 
+            return false;
+    }
+    
+    return true
 
     
-// }
-function deepEqual(x, y) {
-    if (x === y) return true;
-
-    if (x == null || y == null) return false;
-
-    if (typeof x !== 'object' || typeof y !== 'object') {
-        return false;
-    }
-
-    let keysX = Object.keys(x);
-    let keysY = Object.keys(y);
-
-    if (keysX.length !== keysY.length) return false;
-
-    for (let key of keysX) {
-        if (!keysY.includes(key) || !deepEqual(x[key], y[key])) {
-            return false;
-        }
-    }
-
-    return true;
 }
 
 let names = {
@@ -57,5 +25,5 @@ let names = {
     nullValue = null,
     string = 'babe';
 
-console.log(deepEqual(names, names))
+console.log(deepEqual(string, names))
 
--- exercises/chapter-4/test.js
+++ exercises/chapter-4/test.js
@@ -50,9 +50,6 @@ function phi(table) {
               (table[0] + table[2]));
 }
 console.log(phi([76, 9, 4, 1]).toFixed(4));
-
-*/
-
 let journal = [];
 function addEntry(events, squirrel) {
   journal.push({events, squirrel});
@@ -62,3 +59,48 @@ addEntry(["work", "ice cream", "cauliflower", "lasagna",
 "touched tree", "brushed teeth"], false);
 addEntry(["weekend", "cycling", "break", "peanuts","beer"], true);
 
+let names = {
+    id: '1',
+    name: 'Smaller'
+}, 
+    nullValue = null,
+    string = 'babe';
+function deepEqual(x, y) {
+    if (x === y) return true;
+
+    if (x == null || y == null) return false;
+
+    if (typeof x !== 'object' || typeof y !== 'object') {
+        return false;
+    }
+
+    let keysX = Object.keys(x);
+    let keysY = Object.keys(y);
+
+    if (keysX.length !== keysY.length) return false;
+
+    for (let key of keysX) {
+        if (!keysY.includes(key) || !deepEqual(x[key], y[key])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+deepEqual(string, names)
+*/
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return find(current + 5, `(${history} + 5)`) ??
+      find(current * 3, `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
+}
+console.log(findSolution(14));
+// → (((1 * 3) + 5) * 3)
